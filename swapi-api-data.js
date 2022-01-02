bcf611af0b21f960a72a91e0d4265e5b114224b5
BrianStritch <brians12ca18@gmail.com>
2022-01-02 00:15:54 +0000

    initial commit

---
+++ swapi-api-data.js
@@ -0,0 +1,15 @@
+{
+    "people": "https://ci-swapi.herokuapp.com/api/people/",
+    "planets": "https://ci-swapi.herokuapp.com/api/planets/",
+    "films": "https://ci-swapi.herokuapp.com/api/films/",
+    "species": "https://ci-swapi.herokuapp.com/api/species/",
+    "vehicles": "https://ci-swapi.herokuapp.com/api/vehicles/",
+    "starships": "https://ci-swapi.herokuapp.com/api/starships/"
+}
+
+/**
+ * 
+ *  to get the data from each api you need to input curl https://ci-swapi.herokuapp.com/api/-----/
+ *  the ---- being the file you want and follow with a /  ie: curl https://ci-swapi.herokuapp.com/api/people/
+ * to save it to a file you put curl https://ci-swapi.herokuapp.com/api/people > file-name-you-want.js
+ */
